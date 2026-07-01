c7de2ad319740c1183e5a9a6d106693e0fe86365
huy5.tran <huy5.tran@lge.com>
2026-07-01 13:30:10 +0700

    260701

--- Clock Devices/solve.py
+++ Clock Devices/solve.py
@@ -32,7 +32,26 @@ def solve():
                 parent[v] = u
                 stack.append(v)
 
-    
+    dp = [None] * N
+
+    for u in reversed(order):
+        # Nếu đỉnh u chọn nhãn need[u] thì nó sẽ đc W[u]
+        # Chọn nhãn khác thì k đc gì
+        cur = [0] * 12
+        cur[need[u]] = W[u]
+
+        for v in adj[u]:
+            if parent[v] == u:
+                # Cắt cạnh u-v
+                best_cut = max(dp[v]) - C
+                # Giữ cạnh u-v
+                for label in range(12):
+                    cur[label] += max(dp[v][label], best_cut)
+
+        dp[u] = cur
+
+    print(max(dp[0]))
+
 
 if __name__ == "__main__":
     solve()
